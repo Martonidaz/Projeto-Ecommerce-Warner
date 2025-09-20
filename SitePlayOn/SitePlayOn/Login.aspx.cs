62f85b9f87130601d88aadc03f4b882a25af618c
Brenooryan <breno_ryan2014@outlook.com>
2025-09-19 21:24:47 -0300

    Criação das paginas
    Criação das paginas Contato, Categoria, Produtos
    
    Criação das MasterPage do admin e do cliente

---
+++ SitePlayOn/SitePlayOn/Login.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+string connStr = ConfigurationManager.ConnectionStrings["MinhaConexao"].ConnectionString;
+
+using (SqlConnection con = new SqlConnection(connStr))
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
