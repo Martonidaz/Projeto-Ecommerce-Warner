b948334f70859ecf5f84cd7d9622d650ba73a5f9
Brenooryan <breno_ryan2014@outlook.com>
2025-09-23 21:28:09 -0300

    Tabela de visualização de usuarios
    Tabela de visualização de usuarios com delete alteração

---
+++ SitePlayOn/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿SELECT u.id_usuario, u.username, c.nome, c.cpf, c.sexo, c.data_nascimento, c.telefone, c.email, c.data_cadastro, u.tipousuario FROM Usuario u INNER JOIN Cliente c On u.id_cliente = c.id_cliente
+
+INSERT INTO Cliente (nome, cpf, sexo, data_nascimento, telefone, email, data_cadastro) VALUES ('BRENO', '12345678910', 'M', '2002-07-06', '12991883920','breno@ryan','2025-09-23')
+
+INSERT INTO Usuario (username, senha,id_cliente, tipousuario) VALUES ('Breno', '123', 17, 'admin')
+SELECT * FROM Cliente
+SELECT * FROM Usuario
+
+
+DROP TABLE 
+
+DELETE FROM Cliente WHERE id_cliente = 16;
+DELETE FROM Usuario WHERE id_usuario = 6;
--- SitePlayOn/SitePlayOn/visualizar_usuario.aspx.cs
+++ SitePlayOn/SitePlayOn/visualizar_usuario.aspx.cs
@@ -24,7 +24,7 @@ public partial class visualizar_usuario : System.Web.UI.Page
 
         using (SqlConnection con = new SqlConnection(conexao))
         {
-            string sqlVerifica = @"SELECT id_cliente, nome, cpf, sexo, data_nascimento, telefone, email, data_cadastro FROM Cliente";
+            string sqlVerifica = @"SELECT u.id_usuario, u.username, c.nome, c.cpf, c.sexo, c.telefone, c.email, u.tipousuario FROM Usuario u INNER JOIN Cliente c On u.id_cliente = c.id_cliente";
             
             SqlCommand cmdVerifica = new SqlCommand(sqlVerifica, con);
             con.Open();
@@ -33,5 +33,85 @@ public partial class visualizar_usuario : System.Web.UI.Page
             gridUsuarios.DataSource = dr;
             gridUsuarios.DataBind();
         }
+    }
+
+    protected void gridUsuarios_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+    {
+        gridUsuarios.EditIndex = e.NewEditIndex;
+        visualizarUsuarios();
+    }
+
+    protected void gridUsuarios_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+    {
+        gridUsuarios.EditIndex = -1;
+        visualizarUsuarios();
+    }
+
+    protected void gridUsuarios_RowUpdating(object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+    {
+        int id_usuario = Convert.ToInt32(gridUsuarios.DataKeys[e.RowIndex].Value);
+        var row = gridUsuarios.Rows[e.RowIndex];
+
+        string username = ((System.Web.UI.WebControls.TextBox)row.Cells[1].Controls[0]).Text;
+        string nome = ((System.Web.UI.WebControls.TextBox)row.Cells[2].Controls[0]).Text;
+        string cpf = ((System.Web.UI.WebControls.TextBox)row.Cells[3].Controls[0]).Text;
+        string sexo = ((System.Web.UI.WebControls.TextBox)row.Cells[4].Controls[0]).Text;
+        string telefone = ((System.Web.UI.WebControls.TextBox)row.Cells[5].Controls[0]).Text;
+        string email = ((System.Web.UI.WebControls.TextBox)row.Cells[6].Controls[0]).Text;
+        string tipousuario = ((System.Web.UI.WebControls.TextBox)row.Cells[7].Controls[0]).Text;
+
+        string conexao = ConfigurationManager.ConnectionStrings["BancoLoja"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(conexao))
+        {
+            con.Open();
+
+            // Atualiza Usuario
+            string sqlUsuario = "UPDATE Usuario SET username=@username, tipousuario=@tipousuario WHERE id_usuario=@id_usuario";
+            SqlCommand cmdUsuario = new SqlCommand(sqlUsuario, con);
+            cmdUsuario.Parameters.AddWithValue("@Username", username);
+            cmdUsuario.Parameters.AddWithValue("@tipousuario", tipousuario);
+            cmdUsuario.Parameters.AddWithValue("@id_usuario", id_usuario);
+            cmdUsuario.ExecuteNonQuery();
+
+            // Atualiza Cliente
+            string sqlCliente = @"UPDATE Cliente SET nome=@nome, email=@email, cpf=@cpf, sexo=@sexo, telefone=@telefone
+                                      WHERE id_cliente = (SELECT id_cliente FROM Usuario WHERE id_usuario=@id_usuario)";
+            SqlCommand cmdCliente = new SqlCommand(sqlCliente, con);
+            cmdCliente.Parameters.AddWithValue("@nome", nome);
+            cmdCliente.Parameters.AddWithValue("@email", email);
+            cmdCliente.Parameters.AddWithValue("@cpf", cpf);
+            cmdCliente.Parameters.AddWithValue("@sexo", sexo);
+            cmdCliente.Parameters.AddWithValue("@telefone", telefone);
+            cmdCliente.Parameters.AddWithValue("@id_usuario", id_usuario);
+            cmdCliente.ExecuteNonQuery();
+        }
+
+        gridUsuarios.EditIndex = -1;
+        visualizarUsuarios();
+    }
+
+    protected void gridUsuarios_RowDeleting(object sender, System.Web.UI.WebControls.GridViewDeleteEventArgs e)
+    {
+        int id_usuario = Convert.ToInt32(gridUsuarios.DataKeys[e.RowIndex].Value);
+
+        string conexao = ConfigurationManager.ConnectionStrings["BancoLoja"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(conexao))
+        {
+            con.Open();
+
+            // Primeiro deleta Cliente
+            string sqlCliente = @"DELETE FROM Cliente WHERE id_cliente = (SELECT id_cliente FROM Usuario WHERE id_usuario=@id_usuario)";
+            SqlCommand cmdCliente = new SqlCommand(sqlCliente, con);
+            cmdCliente.Parameters.AddWithValue("id_usuario", id_usuario);
+            cmdCliente.ExecuteNonQuery();
+
+            // Depois deleta Usuario
+            string sqlUsuario = "DELETE FROM Usuario WHERE id_usuario=@id_usuario";
+            SqlCommand cmdUsuario = new SqlCommand(sqlUsuario, con);
+            cmdUsuario.Parameters.AddWithValue("@id_usuario", id_usuario);
+            cmdUsuario.ExecuteNonQuery();
+        }
+
+        visualizarUsuarios();
     }
 }
